66e2c0f28600b614df33fb815aa7eda29d5ea3e3
Lee Borlace <lee.borlace@ignia.com.au>
2019-09-09 08:58:57 +0800

    Update lee-attempt.py

--- udemy-nlp-lazyprogrammer/3-spam-detector/lee-attempt.py
+++ udemy-nlp-lazyprogrammer/3-spam-detector/lee-attempt.py
@@ -6,38 +6,78 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import AdaBoostClassifier
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+def DoTfidf():
+    # Fit and transform from sentences (input, i.e. X) to vocab array.
+    print("** TfidfVectorizer **");
+    
+    obj = TfidfVectorizer()
+    X = obj.fit_transform(sentences)
+    
+    # Convert 1st column into an array where 'spam' = 1, of 'ham' = 0. Probably a more elegant way to do this!
+    rawColumn = data[:,0]
+    Y = []
+    for rawOrigin in rawColumn:
+        if rawOrigin == 'spam' :
+            Y.append(1)
+        else:
+            Y.append(0)
+    
+    testNumber = 1000
+    Xtrain = X[:-testNumber,]
+    Ytrain = np.array(Y)[:-testNumber,]
+    Xtest = X[-testNumber:,]
+    Ytest = np.array(Y)[-testNumber:,]    
+    
+    # Method 1
+    model = MultinomialNB()
+    model.fit(Xtrain, Ytrain)
+    print("Classification rate for NB:", model.score(Xtest, Ytest))
+    
+    # Method 2
+    model = AdaBoostClassifier()
+    model.fit(Xtrain, Ytrain)
+    print("Classification rate for AdaBoost:", model.score(Xtest, Ytest))
+
+
+
+def DoCountVectorizer():
+    # Fit and transform from sentences (input, i.e. X) to vocab array.
+    print("** CountVectorizer **");
+    
+    obj = CountVectorizer()
+    X = obj.fit_transform(sentences)
+    
+    # Convert 1st column into an array where 'spam' = 1, of 'ham' = 0. Probably a more elegant way to do this!
+    rawColumn = data[:,0]
+    Y = []
+    for rawOrigin in rawColumn:
+        if rawOrigin == 'spam' :
+            Y.append(1)
+        else:
+            Y.append(0)
+    
+    testNumber = 1000
+    Xtrain = X[:-testNumber,]
+    Ytrain = np.array(Y)[:-testNumber,]
+    Xtest = X[-testNumber:,]
+    Ytest = np.array(Y)[-testNumber:,]    
+    
+    # Method 1
+    model = MultinomialNB()
+    model.fit(Xtrain, Ytrain)
+    print("Classification rate for NB:", model.score(Xtest, Ytest))
+    
+    # Method 2
+    model = AdaBoostClassifier()
+    model.fit(Xtrain, Ytrain)
+    print("Classification rate for AdaBoost:", model.score(Xtest, Ytest))
+
 
 # Get raw data. Sentences is a 1D array of the 2nd column of all data
 data = pd.read_csv('spam.csv', encoding = "ISO-8859-1").values
 np.random.shuffle(data)
 sentences = data[:,1]
-
-# Fit and transform from sentences (input, i.e. X) to vocab array.
-obj = TfidfVectorizer()
-X = obj.fit_transform(sentences)
-
-# Convert 1st column into an array where 'spam' = 1, of 'ham' = 0. Probably a more elegant way to do this!
-rawColumn = data[:,0]
-Y = []
-for rawOrigin in rawColumn:
-    if rawOrigin == 'spam' :
-        Y.append(1)
-    else:
-        Y.append(0)
-
-testNumber = 2000
-Xtrain = X[:-testNumber,]
-Ytrain = np.array(Y)[:-testNumber,]
-Xtest = X[-testNumber:,]
-Ytest = np.array(Y)[-testNumber:,]    
-
-# Method 1
-model = MultinomialNB()
-model.fit(Xtrain, Ytrain)
-print("Classification rate for NB:", model.score(Xtest, Ytest))
-
-# Method 2
-model = AdaBoostClassifier()
-model.fit(Xtrain, Ytrain)
-print("Classification rate for AdaBoost:", model.score(Xtest, Ytest))
+DoTfidf()
+DoCountVectorizer()
