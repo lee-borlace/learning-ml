75e1fd2a4d1cd9c20c4aee7bcd875e6ac1a4bc4e
Lee Borlace <lee.borlace@ignia.com.au>
2019-09-24 17:32:07 +0800

    Spacy learnings...

--- spacy/readme.md
+++ spacy/readme.md
@@ -1,12 +1,17 @@
 # Info
 https://spacy.io/usage
+https://github.com/huggingface/neuralcoref
 
 # Getting started
-`pip install -U spacy`
-`pip install -U neuralcoref`
+`pip install spacy==2.1.0`
+`pip install neuralcoref --no-binary neuralcoref`
+
+See these issues as to why spacy and neuralcoref needed to be installed that way -
+
+https://github.com/huggingface/neuralcoref/issues/158
+https://github.com/huggingface/neuralcoref/issues/103
+https://github.com/explosion/spaCy/issues/2798
+
+Several differently-sized language models -
 `python -m spacy download en_core_web_s`
 `python -m spacy download en_core_web_lg`
-
-Might need the following if there's an error such as 'size changed, may indicate binary incompatibility' when running with neural coref
-`pip uninstall neuralcoref`
-`pip install neuralcoref --no-binary neuralcoref`
--- spacy/spacy_test_1.py
+++ spacy/spacy_test_1.py
@@ -1,10 +1,15 @@
 import spacy
 
-# MODEL = "en_core_web_sm"
+# Comment in a large or small model depending on how long you want to wait :)
+#MODEL = "en_core_web_sm"
 MODEL = "en_core_web_lg"
 
 # Load English tokenizer, tagger, parser, NER and word vectors
+print(f"Loading spaCy model {MODEL}...")
 nlp = spacy.load(MODEL)
+print("Done")
+
+print("************ Mucking around with parsing ************")
 
 # Add neural coref to SpaCy's pipe
 import neuralcoref
@@ -18,9 +23,32 @@ neuralcoref.add_to_pipe(nlp)
 #         "worth talking to,” said Thrun, in an interview with Recode earlier "
 #         "this week.")
 
-text = "The man chased the boy with his gun"
-doc = nlp(text)
+# text = "The man chased the boy with his arm"
+text = "what did the green cat eat?"
 
+doc = nlp(text)
 
 for token in doc:
     print(f"TEXT={token.text},LEMMA={token.lemma_},POS={token.pos_},TAG={token.tag_},DEP={token.dep_}")
+
+
+
+
+print("************ Mucking around with co-reference ************")
+
+print(f"Coref clusters : {doc._.coref_clusters}")
+print(f"Doc with resolved references : '{doc._.coref_resolved}'")
+
+
+
+
+
+
+
+print("************ Mucking around with vector similarity ************")
+doc1 = nlp("I want to order a bike")
+doc2 = nlp("I'd like to request a bicycle")
+doc3 = nlp("quack quack quack")
+
+print(f"Similarity between '{doc1}' and '{doc2}' : {doc1.similarity(doc2)}")
+print(f"Similarity between '{doc1}' and '{doc3}' : {doc1.similarity(doc3)}")
