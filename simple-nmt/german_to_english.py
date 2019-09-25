9c0074cd136d50ef99f2f1648cd2d95bea08bff0
Lee Borlace <lee.borlace@ignia.com.au>
2019-09-25 17:28:35 +0800

    Initial simple nmt experiments

---
+++ simple-nmt/german_to_english.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Import Required Libraries
+# https://github.com/prateekjoshi565/machine_translation
+
+# In[1]:
+
+
+import string
+import re
+from numpy import array, argmax, random, take
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Embedding, Bidirectional, RepeatVector, TimeDistributed
+from keras.preprocessing.text import Tokenizer
+from keras.callbacks import ModelCheckpoint
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from keras import optimizers
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+pd.set_option('display.max_colwidth', 200)
+
+
+# ### Read Data
+
+# Our data is a text file of English-German sentence pairs. First we will read the file using the function defined below.
+
+# In[2]:
+
+
+# function to read raw text file
+def read_text(filename):
+    # open the file
+    file = open(filename, mode='rt', encoding='utf-8')
+    # read all text
+    text = file.read()
+    file.close()
+    return text
+
+
+# Now let's define a function to split the text into English-German pairs separated by '\n' and then split these pairs into English sentences and German sentences.
+
+# In[3]:
+
+
+# split a text into sentences
+def to_lines(text):
+    sents = text.strip().split('\n')
+    sents = [i.split('\t') for i in sents]
+    return sents
+
+
+# __Download the data from [here.](http://www.manythings.org/anki/deu-eng.zip)__ and extract "deu.txt" in your working directory.
+
+# In[4]:
+
+
+data = read_text("deu.txt")
+deu_eng = to_lines(data)
+deu_eng = array(deu_eng)
+
+
+# The actual data contains over 150,000 sentence-pairs. However, we will use the first 50,000 sentence pairs only to reduce the training time of the model. You can change this number as per you system computation power.
+
+# In[5]:
+
+
+deu_eng = deu_eng[:50000,:]
+
+
+# ### Text Pre-Processing
+
+# #### Text Cleaning
+# 
+# Let's take a look at our data, then we will decide which pre-processing steps to adopt.
+
+# In[6]:
+
+
+deu_eng
+
+
+# We will get rid of the punctuation marks, and then convert the text to lower case.
+
+# In[7]:
+
+
+# Remove punctuation
+deu_eng[:,0] = [s.translate(str.maketrans('', '', string.punctuation)) for s in deu_eng[:,0]]
+deu_eng[:,1] = [s.translate(str.maketrans('', '', string.punctuation)) for s in deu_eng[:,1]]
+
+
+# In[8]:
+
+
+deu_eng
+
+
+# In[9]:
+
+
+# convert to lowercase
+for i in range(len(deu_eng)):
+    deu_eng[i,0] = deu_eng[i,0].lower()
+    
+    deu_eng[i,1] = deu_eng[i,1].lower()
+
+
+# In[10]:
+
+
+deu_eng
+
+
+# #### Text to Sequence Conversion
+# 
+# To feed our data in a Seq2Seq model, we will have to convert both the input and the output sentences into integer sequences of fixed length. Before that, let's visualise the length of the sentences. We will capture the lengths of all the sentences in two separate lists for English and German, respectively.
+
+# In[11]:
+
+
+# empty lists
+eng_l = []
+deu_l = []
+
+# populate the lists with sentence lengths
+for i in deu_eng[:,0]:
+    eng_l.append(len(i.split()))
+
+for i in deu_eng[:,1]:
+    deu_l.append(len(i.split()))
+
+
+# In[12]:
+
+
+length_df = pd.DataFrame({'eng':eng_l, 'deu':deu_l})
+
+
+# In[13]:
+
+
+length_df.hist(bins = 30)
+plt.show()
+
+
+# The maximum length of the German sentences is 11 and that of the English phrases is 8.
+
+# Let's vectorize our text data by using Keras's Tokenizer() class. It will turn our sentences into sequences of integers. Then we will pad those sequences with zeros to make all the sequences of same length.
+
+# In[14]:
+
+
+# function to build a tokenizer
+def tokenization(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+
+# In[15]:
+
+
+# prepare english tokenizer
+eng_tokenizer = tokenization(deu_eng[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+
+eng_length = 8
+print('English Vocabulary Size: %d' % eng_vocab_size)
+
+
+# In[16]:
+
+
+# prepare Deutch tokenizer
+deu_tokenizer = tokenization(deu_eng[:, 1])
+deu_vocab_size = len(deu_tokenizer.word_index) + 1
+
+deu_length = 8
+print('Deutch Vocabulary Size: %d' % deu_vocab_size)
+
+
+# Given below is a function to prepare the sequences. It will also perform sequence padding to a maximum sentence length as mentioned above. 
+
+# In[17]:
+
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+    # integer encode sequences
+    seq = tokenizer.texts_to_sequences(lines)
+    # pad sequences with 0 values
+    seq = pad_sequences(seq, maxlen=length, padding='post')
+    return seq
+
+
+# ### Model Building
+
+# We will now split the data into train and test set for model training and evaluation, respectively.
+
+# In[18]:
+
+
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(deu_eng, test_size=0.2, random_state = 12)
+
+
+# It's time to encode the sentences. We will encode German sentences as the input sequences and English sentences as the target sequences. It will be done for both train and test datasets.
+
+# In[19]:
+
+
+# prepare training data
+trainX = encode_sequences(deu_tokenizer, deu_length, train[:, 1])
+trainY = encode_sequences(eng_tokenizer, eng_length, train[:, 0])
+
+
+# In[20]:
+
+
+# prepare validation data
+testX = encode_sequences(deu_tokenizer, deu_length, test[:, 1])
+testY = encode_sequences(eng_tokenizer, eng_length, test[:, 0])
+
+
+# Now comes the exciting part! Let us define our Seq2Seq model architecture. We are using an Embedding layer and an LSTM layer as our encoder and another LSTM layer followed by a Dense layer as the decoder.  
+
+# In[23]:
+
+
+# build NMT model
+def build_model(in_vocab, out_vocab, in_timesteps, out_timesteps, units):
+    model = Sequential()
+    model.add(Embedding(in_vocab, units, input_length=in_timesteps, mask_zero=True))
+    model.add(LSTM(units))
+    model.add(RepeatVector(out_timesteps))
+    model.add(LSTM(units, return_sequences=True))
+    model.add(Dense(out_vocab, activation='softmax'))
+    return model
+
+
+# We are using RMSprop optimizer in this model as it is usually a good choice for recurrent neural networks.
+
+# In[25]:
+
+
+model = build_model(deu_vocab_size, eng_vocab_size, deu_length, eng_length, 512)
+rms = optimizers.RMSprop(lr=0.001)
+model.compile(optimizer=rms, loss='sparse_categorical_crossentropy')
+
+
+# Please note that we have used __'sparse_categorical_crossentropy'__ as the loss function because it allows us to use the target sequence as it is instead of one hot encoded format. One hot encoding the target sequences with such a huge vocabulary might consume our system's entire memory.
+
+# It seems we are all set to start training our model. We will train it for 30 epochs and with a batch size of 512. You may change and play these hyperparameters. We will also be using __ModelCheckpoint()__ to save the best model with lowest validation loss. I personally prefer this method over early stopping.
+
+# In[26]:
+
+
+filename = 'model.h1.24_jan_19'
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+history = model.fit(trainX, trainY.reshape(trainY.shape[0], trainY.shape[1], 1), 
+          epochs=30, batch_size=512, 
+          validation_split = 0.2,
+          callbacks=[checkpoint], verbose=1)
+
+
+# Let's compare the training loss and the validation loss.
+
+# In[27]:
+
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['train','validation'])
+plt.show()
+
+
+# ### Make Predictions
+
+# Let's load the saved model to make predictions.
+
+# In[21]:
+
+
+model = load_model('model.h1.24_jan_19')
+preds = model.predict_classes(testX.reshape((testX.shape[0],testX.shape[1])))
+
+
+# In[22]:
+
+
+def get_word(n, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == n:
+            return word
+    return None
+
+
+# In[23]:
+
+
+# convert predictions into text (English)
+preds_text = []
+for i in preds:
+    temp = []
+    for j in range(len(i)):
+        t = get_word(i[j], eng_tokenizer)
+        if j > 0:
+            if (t == get_word(i[j-1], eng_tokenizer)) or (t == None):
+                temp.append('')
+            else:
+                temp.append(t)
+             
+        else:
+            if(t == None):
+                temp.append('')
+            else:
+                temp.append(t)            
+        
+    preds_text.append(' '.join(temp))
+
+
+# In[24]:
+
+
+pred_df = pd.DataFrame({'actual' : test[:,0], 'predicted' : preds_text})
+
+
+# In[25]:
+
+
+pd.set_option('display.max_colwidth', 200)
+
+
+# In[34]:
+
+
+pred_df.head(15)
+
+
+# In[35]:
+
+
+pred_df.tail(15)
+
+
+# In[37]:
+
+
+pred_df.tail(15)
+
+
+# In[40]:
+
+
+pred_df.sample(15)
+
